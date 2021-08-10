148c3f95fc46a92d4516a7651938d9db98e967e8
fukky21 <r.fukky21@gmail.com>
2021-08-11 06:40:00 +0900

    feat: getThemes, addThemeを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,60 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+export const getThemes = functions.region("asia-northeast1").https.onRequest(async (_, res) => {
+  try {
+    // 利用者数をカウントする
+    await admin.firestore().runTransaction(async (transaction) => {
+      const ref = admin.firestore().collection("logs").doc("userCount");
+      const doc = await transaction.get(ref);
+
+      if (!doc.exists) {
+        throw new Error("Document does not exist");
+      }
+
+      const data = doc.data();
+      if (data === undefined) {
+        throw new Error("data is undefined");
+      }
+
+      const newCount: number = data.count + 1;
+      transaction.update(ref, {
+        count: newCount,
+        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+      });
+    });
+
+    // お題をすべて取得してデータを返す
+    const themes: string[] = [];
+    await admin.firestore().collection("themes").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        themes.push(data.body);
+      });
+    });
+    res.status(200).json({themes: themes});
+    return;
+  } catch (e) {
+    functions.logger.error(e);
+    return;
+  }
+});
+
+export const addTheme = functions.region("asia-northeast1").https.onRequest(async (req, res) => {
+  try {
+    const body: string = req.body.body;
+    const now = admin.firestore.FieldValue.serverTimestamp();
+    await admin.firestore().collection("themes").add({
+      body: body,
+      createdAt: now,
+      updatedAt: now,
+    });
+    res.status(201).json({status: "OK"});
+    return;
+  } catch (e) {
+    functions.logger.error(e);
+    return;
+  }
 });

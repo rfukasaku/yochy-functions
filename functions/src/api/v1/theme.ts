36ccc7936cc840864920f109fd42d2ef69e88968
fukky21 <r.fukky21@gmail.com>
2021-09-14 05:59:10 +0900

    feat: userCountを記録するのを廃止する

--- functions/src/api/v1/theme.ts
+++ functions/src/api/v1/theme.ts
@@ -3,27 +3,6 @@ import { Request, Response } from "express";
 
 export const getThemes = async (_: Request, res: Response): Promise<void> => {
   try {
-    // 利用者数をカウントする
-    await admin.firestore().runTransaction(async (transaction) => {
-      const ref = admin.firestore().collection("logs").doc("userCount");
-      const doc = await transaction.get(ref);
-
-      if (!doc.exists) {
-        throw new Error("Document does not exist");
-      }
-
-      const data = doc.data();
-      if (data === undefined) {
-        throw new Error("data is undefined");
-      }
-
-      const newCount: number = data.count + 1;
-      transaction.update(ref, {
-        count: newCount,
-        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-      });
-    });
-
     // お題をすべて取得してデータを返す
     const themes: string[] = [];
     await admin.firestore().collection("themes").get().then((querySnapshot) => {
